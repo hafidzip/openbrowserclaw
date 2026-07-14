09ee2130d62162c6d41211eb716a21f4c775dede
Hafidz Ihza Pratama <contact@antitesa.com>
2026-07-14 10:33:48 +0700

    update

--- openchad-react/Container.tsx
+++ openchad-react/Container.tsx
@@ -520,6 +520,9 @@ export default function Container({ Apps }: { Apps: Project }) {
         data = await res.json();
       }
       (window as any).BASE_URL = data.BASE_URL;
+      
+      window.dispatchEvent(new CustomEvent('websocket-url-change', { detail: data.BASE_URL }));
+
       (window as any).PROJECT_ROOT = data.PROJECT_ROOT;
       (window as any).PYTHON_ROOT = data.PYTHON_ROOT;
       (window as any).BACKENDS_DIR = data.BACKENDS_DIR;
--- openchad-react/components/useWebSocket/index.ts
+++ openchad-react/components/useWebSocket/index.ts
@@ -1,9 +1,21 @@
+import { useEffect, useState } from "react";
 import type { EventHandler } from "../usePython";
 import type { UseWebSocketReturn } from "./useWebSocket";
 import { useWebSocketSingleton, useWebSocketEvent as useWebSocketEventImpl } from "./useWebSocket";
 
 export function useWebSocket<T>(): UseWebSocketReturn<T> {
-    const url = typeof window !== 'undefined' ? `ws${window.location.protocol === 'https:' ? 's' : ''}://${(window as any).BASE_URL}/ws` : "ws://localhost:3000/ws";
+    const [url, setUrl] = useState(typeof window !== 'undefined' ? `ws${window.location.protocol === 'https:' ? 's' : ''}://${(window as any).BASE_URL}/ws` : "ws://localhost:3000/ws");
+    
+    useEffect(() => {
+        const handler = (e: any) => {
+            setUrl(e.detail);
+        };
+        window.addEventListener('websocket-url-change', handler);
+        return () => {
+            window.removeEventListener('websocket-url-change', handler);
+        }
+    }, []);
+
     return useWebSocketSingleton<T>(url);
 }
 
